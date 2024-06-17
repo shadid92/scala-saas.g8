647ebe1b7dd522078f6ee048834c79be6f001f57
Shadid <shadid1992@hotmail.com>
2024-06-17 23:29:18 +0300

    fixes

--- src/main/scala/$package$/core/users/UserId.scala
+++ src/main/scala/$package$/core/users/UserId.scala
@@ -4,7 +4,7 @@ import $package$.prelude.{*, given}
 import $package$.core.util.TaggedUUID
 import $package$.core.util.TaggedUUIDCompanion
 
-case class UserId(override val uuid: UUID) extends TaggedUUID["user"](uuid)
+case class UserId(override val uuid: UUID) extends TaggedUUID["user"]
 object UserId extends TaggedUUIDCompanion["user", UserId] {
   override def fromUUID(uuid: UUID) = UserId(uuid)
 }
